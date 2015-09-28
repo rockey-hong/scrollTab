c32bc1a79d98ce271f2d565115b14e30e6c2feab
honglujia <honglujia93@163.com>
2015-09-29 01:37:12 +0800

    #修正scrollbar的坐标位置,鼠标进入进出的事件。 #把一部分的代码弄成一个函数块，方便维护
    Signed-off-by: honglujia <honglujia93@163.com>

--- scrollBar.js
+++ scrollBar.js
@@ -2,40 +2,78 @@
 var scrollBar = (function() {
 
     function createBar(ele, cross, show) {
-
+        //构建scrollBar div结构
         var scrollDom = createDivDom(ele, cross);
-        
+
         ele.setAttribute('data-mouse', 'out');
-        
+
+        //设置显示show参数的时候显示bar
         if (show) {
             scrollDom.style.display = 'block';
         }
 
+        //鼠标事件。
+        mouseBarEvent(ele, show, scrollDom);
+
+        if (!ele.parentNode.style.position) {
+            ele.parentNode.style.position = 'relative';
+        }
+
+        ele.parentNode.appendChild(scrollDom);
+    }
+
+    // 鼠标进入进出事件函数
+    function mouseBarEvent(ele, show, scrollDom) {
+
+        // !show是为了防止 show参数设置true的时候触发隐藏显示事件。
         ele.addEventListener('mouseenter', function () {
             if (!show) {
                 scrollDom.style.display = 'block';
             }
             ele.setAttribute('data-mouse', 'on');
-        }, false); 
+        }, false);
 
         ele.addEventListener('mouseleave', function () {
             if (!show) {
                 scrollDom.style.display = 'none';
             }
             ele.setAttribute('data-mouse', 'out');
-        },false);
+        }, false);
 
-        scrollDom.style.display = 'block';
-        ele.appendChild(scrollDom);
+        //进入滚动条的时候滚动条不要消失
+        scrollDom.addEventListener('mouseenter', function () {
+            if (!show) {
+                scrollDom.style.display = 'block';
+            }
+            ele.setAttribute('data-mouse', 'on');
+        }, false);
+
+        scrollDom.addEventListener('mouseleave', function () {
+            if (!show) {
+                scrollDom.style.display = 'none';
+            }
+            ele.setAttribute('data-mouse', 'out');
+        }, false);
+
     }
 
     function createDivDom(ele, cross) {
 
         var barBox = document.createElement('div');
         var scrollBar = document.createElement('div');
+
+        setBarStyle(ele, barBox, scrollBar, cross);
+
+        barBox.appendChild(scrollBar);
+        scrollBarEvent(ele, scrollBar, cross);
+        return barBox;
+    }
+
+    function setBarStyle(ele, barBox, scrollBar, cross, color) {
+
         barBox.className = 'scrollBar-box';
         scrollBar.className = 'scrollBar-tap';
-        
+
         barBox.style.background  = 'rgba(51, 51, 51, 0.3)';
         barBox.style.borderRadius = '5px';
         barBox.style.position = 'absolute';
@@ -48,27 +86,36 @@ var scrollBar = (function() {
         scrollBar.style.position = 'absolute';
         scrollBar.style.cursor = 'pointer';
 
+        // cross true 横向 false 纵向 默认 false
         if (cross) {
              barBox.style.width = ele.clientWidth + 'px';
              barBox.style.height = '10px';
-             barBox.style.left = '0px';
-             barBox.style.bottom = '0px';
-
+             console.log(getStyle(ele.parentNode, 'marginLeft', true));
+             barBox.style.left = ele.offsetLeft - getStyle(ele.parentNode, 'marginLeft', true) + 1 + 'px';
+             barBox.style.top = ele.offsetTop + ele.clientHeight - 8
+                                - getStyle(ele.parentNode, 'marginTop', true) + 'px';
              scrollBar.style.height = '8px';
              scrollBar.style.width = ele.clientWidth *ele.clientWidth / ele.scrollWidth + 'px';
         } else {
             barBox.style.height = ele.clientHeight + 'px';
             barBox.style.width = '10px';
-            barBox.style.top = '0px';
-            barBox.style.right= '0px';
-
+            barBox.style.left = ele.offsetLeft + ele.clientWidth - 8
+                                - getStyle(ele.parentNode, 'marginLeft', true) + 'px';
+            barBox.style.top = ele.offsetTop + - getStyle(ele.parentNode, 'marginTop', true) + 1 + 'px';
             scrollBar.style.width = '8px';
             scrollBar.style.height = ele.clientHeight  * ele.clientHeight / ele.scrollHeight + 'px';
         }
+    }
 
-        barBox.appendChild(scrollBar);
-        scrollBarEvent(ele, scrollBar, cross);
-        return barBox;
+    function getStyle(ele, style, type) {
+
+        var retNum = window.getComputedStyle(ele, null)[style];
+
+        if (type) {
+            retNum = Number(retNum.slice(0, retNum.length - 2));
+        }
+
+        return retNum;
     }
 
     function scrollBarEvent(ele, scrollBar, cross) {
@@ -78,7 +125,7 @@ var scrollBar = (function() {
         var mouseX, mouseY;
 
         scrollBar.addEventListener('mouseenter', function () {
-            this.style.background = '#fbfbfb';     
+            this.style.background = '#fbfbfb';
         })
 
         scrollBar.addEventListener('mouseleave', function () {
@@ -88,7 +135,7 @@ var scrollBar = (function() {
         })
 
         scrollBar.addEventListener('mousedown', function (e) {
-            
+
             mouseX = e.pageX;
             mouseY = e.pageY;
             flag = true;
@@ -107,7 +154,7 @@ var scrollBar = (function() {
                     mouseY = e.pageY;
                 }
             })
-        })       
+        })
 
         scrollBar.addEventListener('mouseup', function () {
             flag = false;
@@ -126,7 +173,7 @@ var scrollBar = (function() {
     function crossEvent (ele, bar, left) {
 
         var scrollLeft = left * ele.scrollWidth / (ele.clientWidth - bar.clientWidth / 4);
-        
+
         left = Number(bar.style.left.slice(0, this.length - 2)) + left;
 
         ele.scrollLeft = ele.scrollLeft + scrollLeft;
@@ -143,8 +190,9 @@ var scrollBar = (function() {
     }
 
     function crossNoneEvent (ele, bar, top) {
+
         var scrollTop = top * ele.scrollHeight / (ele.clientHeight - bar.clientHeight / 4);
-        
+
         top = Number(bar.style.top.slice(0, this.length-2)) + top;
 
         ele.scrollTop = ele.scrollTop + scrollTop;
@@ -161,7 +209,7 @@ var scrollBar = (function() {
     }
 
     function scrollFunc (e, flag, cross, ele, bar) {
-        
+
         var dir = checkDirection(e);
 
         if(ele.getAttribute('data-mouse') == 'out') {
@@ -201,7 +249,7 @@ var scrollBar = (function() {
     }
     /*
     obj Object
-    obj.ele DOM 
+    obj.ele DOM
     obj.cross Boolean  ture 横向 false纵向(默认)
     obj.show Boolean   ture 显示 false不显示(默认)
     */
@@ -211,7 +259,7 @@ var scrollBar = (function() {
         var show = obj.show || false;
         if (!ele) {
             return false;
-        } 
+        }
         createBar(ele, cross, show);
     }
-})(); 
+})();
